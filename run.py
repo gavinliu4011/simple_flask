31139e702b72f31e79f837413ce7bb53f53ce89f
gavinliu4011 <gavinliu4011@163.com>
2018-07-20 14:59:40 +0800

    完成小型应用程序的Flask框架

---
+++ run.py
@@ -0,0 +1,5 @@
+from simple_flask import app
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ simple_flask/views/account.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from flask import render_template
+from flask import request
+
+account = Blueprint('account', __name__)
+
+
+@account.route('/login.html', methods=['GET', "POST"])
+def login():
+    return render_template('login.html')
